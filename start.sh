770b21f0429c68ea145b1aa678a023da2e4681fb
ShikariBaaZ <91068760+ShikariBaaZ@users.noreply.github.com>
2021-10-31 11:36:54 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ShikariBaaZ/Lawless_Fonts_Bot.git /Lawless_Fonts_Bot
+cd /Stylish-Text-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
